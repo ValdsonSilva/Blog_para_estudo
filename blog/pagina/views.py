089fd501cc8a44c0693da4303fb255e23134d165
ValdsonSilva <valdsonmacedo15@gmail.com>
2023-09-22 14:04:38 -0300

    Primeiro commit

---
+++ blog/pagina/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    
+    mensagem = { 
+                'eu' : 'valdson' ,
+                 'namorada' : 'Joelyne meu amor!'
+    }
+    
+    return render(request, 'home.html', mensagem)
+
+
+def contato(request):
+    
+    mensagem = {'mensagem':"Formulário"}
+    
+    return render(request, 'contato.html', mensagem)
